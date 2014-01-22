5f90b52df16c9f082aa6cb062eecf76b6efb9a41
S <s>
2014-01-22 00:22:52 -0800

    {"type":"TI", "runs":"1"}

--- Hailstone.java
+++ Hailstone.java
@@ -10,7 +10,24 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		int n= readInt("Enter a number");
+		int i= 0;
+		while (n != 1) {
+			i++;
+			if (n%2==0) {
+				takeHalf();
+			} else {
+				make3nPlusOne();
+			}
+		}
+		println ("The process took "+i+" to reach 1");
+	}
+	private void takeHalf() {
+		n=n/2;
+	}
+	
+	private void make3nPlusOne() {
+		n=3*n +1;
 	}
 }
 
