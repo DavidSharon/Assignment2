d7848e2070387799433d28f7e01ab9f523ab5efc
S <s>
2014-01-22 19:49:07 -0800

    {"type":"TI", "runs":"2"}

--- FindRange.java
+++ FindRange.java
@@ -11,6 +11,35 @@ import acm.program.*;
 public class FindRange extends ConsoleProgram {
 	public void run() {
 		/* You fill this in */
+		int userInput =0;
+		int userMax=0;
+		int userMin=0;
+		println("This program finds the largest and smallest numbers");
+		println("Enter "+SENTINEL+" anytime to stop program");
+		userInput=getNumFromUser(userInput);
+		while (userInput != SENTINEL) {
+			if (userInput>userMax) {
+				userMax=userInput;
+			}else{
+				if (userInput<userMin){
+					userMin=userInput;
+				}
+			}
+			userInput=getNumFromUser(userInput);
+		}
+		if (userMax==userMin && userMax==SENTINEL) {
+			println (SENTINEL + "entered as first number so program not run");
+		}
+		else{
+			println ("Smallest number: "+userMin);
+			println ("Largest number: "+userMax);
+		}
 	}
-}
+	
+	private int getNumFromUser(int a) {
+		int getNum= readInt ("Please enter an integer: ");
+		return getNum;
+	}
 
+	private static final int SENTINEL=0;
+}
