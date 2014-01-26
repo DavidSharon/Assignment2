3c32505fbe3d6250b7bb46201cb5724ce26296b5
S <s>
2014-01-25 17:36:21 -0800

    {"type":"TI", "runs":"3"}

--- Artistry.java
+++ Artistry.java
@@ -7,22 +7,116 @@
  * Since this program is more freeform than the rest, tell us
  * a bit about it in these comments!
  */
+import acm.graphics.GOval;
+import acm.graphics.GRect;
 import acm.program.*;
+/*Imported util for RandomGenerator Class */
+import acm.util.*;
+import java.awt.Color;
+
+/* You fill this in.  Remember that you must have
+ * 
+ * 1. At least one filled object,
+ * 2. At least two different colors of objects, and
+ * 3. At least three different types of objects (not
+ *    counting the GLabel you use to sign your name).
+ * 
+ * Also, be sure to sign your name in the bottom-right
+ * corner of the canvas. Your name should be flush up
+ * against the bottom-right corner of the label. We
+ * recommend looking at the getDescent() method as a
+ * means for calculating where to position the label.
+ */
 
 public class Artistry extends GraphicsProgram {
 	public void run() {
-		/* You fill this in.  Remember that you must have
-		 * 
-		 * 1. At least one filled object,
-		 * 2. At least two different colors of objects, and
-		 * 3. At least three different types of objects (not
-		 *    counting the GLabel you use to sign your name).
-		 * 
-		 * Also, be sure to sign your name in the bottom-right
-		 * corner of the canvas. Your name should be flush up
-		 * against the bottom-right corner of the label. We
-		 * recommend looking at the getDescent() method as a
-		 * means for calculating where to position the label.
-		 */
+		int numberOfShapes= introRequestParamaters(numberOfShapes);
+		generateShapesBasedOnParamaters(numberOfShapes);
+		signPicture();
 	}
+
+	private int introRequestParamaters(int a) {
+		explainArtIsRandom();
+		return requestNumberOfShapes(a);
+	}
+
+	private void explainArtIsRandom() {
+		for (int i = 0; i < SMALLNUMBERLIMIT; i++) {
+			GLabel artIsRandom = new GLabel("Art is Random", 100, 75);
+			artIsRandom.setFont("SansSerif-36");
+			artIsRandom.setColor(Color.RED);
+			add(artIsRandom);
+			pause (SMALLPAUSE);
+		}	
+	}
+
+	private int requestNumberOfShapes (int a) {
+		cleanScreen();
+		int userShapeInput= readInt ("Enter number of shapes your heart desires: ");
+		println ("I am going to give you 300x that number, i.e., "+ userShapeInput*300);
+		pause(LARGEPAUSE*2);
+		cleanScreen();
+		return (userShapeInput*300);
+	}
+
+	private int generateShapesBasedOnParamaters(int shapes) {
+		int screenWidth =getWidth();
+		int screenHeight= getHeight();
+		for (int i=0; i<shapes; i++){
+			createRect(screenWidth,screenHeight);
+			createOval(screenWidth,screenHeight);
+			pause(SMALLPAUSE);
+		}
+	}
+	
+	private void createRect(int x,int y) {
+		int startRectX= rgen.nextInt(x);
+		int startRectY= rgen.nextInt(y);
+		int endRectX= rgen.nextInt(x);
+		int endRectY= rgen.nextInt(y);
+		GRect rect= new GRect(startRectX,startRectY,endRectX,endRectY);
+		rect.setFilled(true);
+		Color rectColor= rgen.nextColor();
+		rect.setFillColor(Color.BLUE);
+		add(rect);
+		break;
+	}
+
+	private void createOval(int x, int y) {
+		int startOvalX= rgen.nextInt(x);
+		int startOvalY= rgen.nextInt(y);
+		int endOvalX= rgen.nextInt(x);
+		int endOvalY= rgen.nextInt(y);
+		GOval oval= new GOval(startOvalX,startOvalY,endOvalX,endOvalY);
+		oval.setFilled(true);
+		oval.setFillColor(Color.BLUE);
+		add(oval);
+		break;
+	}
+	
+	private void signPicture() {
+		int screenWidth =getWidth();
+		int screenHeight= getHeight();
+		GLabel sign= new GLabel("David Sharon",screenWidth-50,screenHeight-10);
+		add(sign);
+	}
+	
+	private void cleanScreen() {
+		int screenWidth =getWidth();
+		int screenHeight= getHeight();
+		GRect blankPage= new GRect(0, 0, screenWidth, screenHeight);
+		blankPage.setFilled(true);
+		blankPage.setColor(Color.WHITE);
+		add(blankPage);
+	}
+
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	private final static int SMALLNUMBERLIMIT=10;
+	private final static int LARGENUMBERLIMIT=50;
+	private final static int SMALLPAUSE=20;
+	private final static int LARGEPAUSE=1000;	
+
+
+}
 }
