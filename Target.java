049da40cbf5a4abf99fee26e7c07f654ef766d9f
S <s>
2014-01-25 18:13:33 -0800

    {"type":"TI", "runs":"1"}

--- Target.java
+++ Target.java
@@ -13,5 +13,20 @@ import java.awt.*;
 public class Target extends GraphicsProgram {	
 	public void run() {
 		/* You fill this in. */
+		double midXScreen=getWidth()/2;
+		double midYScreen=getHeight()/2;
+		GOval outerCircle= new GOval (midXScreen-LASTCIRCLERADIUS,midYScreen-LASTCIRCLERADIUS,
+									  midXScreen+LASTCIRCLERADIUS,midYScreen+LASTCIRCLERADIUS);
+		outerCircle.setFilled(true);
+		outerCircle.setColor(Color.RED);
+		GOval innerCircle= new GOval (midXScreen-INNERCIRCLERADIUS,midYScreen-INNERCIRCLERADIUS,
+									midXScreen+INNERCIRCLERADIUS,midYScreen+INNERCIRCLERADIUS);
+		outerCircle.setFilled(true);
+		outerCircle.setColor(Color.RED)
 	}
+	
+	private final static double LASTCIRCLERADIUS=72.0;
+	private final static double MIDCIRCLERADIUS=46.8;
+	private final static double INNERCIRCLERADIUS=21.6;
 }
+
